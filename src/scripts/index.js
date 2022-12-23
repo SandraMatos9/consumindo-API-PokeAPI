4a22444954caa88ec9dd7c4eeb00c661a4ab5b0d
SandraMatos9 <matos.sandrarodrigues@email.com>
2022-12-22 23:35:05 -0300

    entrega

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,38 @@
-/* Monte sua lógica aqui */
+
+function render(array) {
+    const mainList = document.querySelector('.listaPokemons')
+  
+    mainList.innerHTML = ''
+  
+    array.forEach( async ({name}) => {
+      const pokerData = await getPokemonById(name)
+
+      console.log(pokerData)
+      const card = criandoCardPokemon(pokerData)
+  
+      mainList.append(card)
+    })
+  }
+ 
+ 
+  function criandoCardPokemon({name,sprites}) {
+    // console.log(pokemon.name)
+    // console.log(pokemon.sprites)
+    const container = document.createElement('li')
+    const image = document.createElement('img')
+    const p = document.createElement('p')
+    
+    container.classList.add('cardPokemon')
+    image.classList.add('imagemPokemon')
+    p.classList.add('nomePokemon')
+
+ 
+  
+    image.src = sprites.other.dream_world.front_default
+    p.innerText = name
+    container.append(image, p)
+  
+    return container
+  }
+  
+  // const {name, img} = element
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,51 @@
-/* Monte a lógica das requisições aqui */
+async function getAllPokemons() {
+    const pokemons = await fetch(`https://pokeapi.co/api/v2/pokemon/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(res => {
+      render(res.results)
+      console.log(res.results )
+      return render
+    })
+    console.log(pokemons.results)
+    return pokemons
+  }
+   getAllPokemons()
+ 
+
+  async function getPokemonById(idPokemon){
+        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`,{
+            
+            method: 'GET',
+            headers: {
+                'Content-Type':'application/json'
+            }
+        })
+      
+        .then(res=>res.json())
+        .then(res =>{
+          
+            return res
+
+        })
+        return pokemon
+    }
+    // console.log(getPokemonById(25))
+
+
+  function renderSearch(){
+    const searchInput = document.querySelector('input')
+    const botaoPesquisa = document.querySelector('#botaoPesquisa')
+
+    botaoPesquisa.addEventListener('click',() =>{
+        getPokemonById(searchInput.value)
+    })
+
+  }
+
+
+ 
